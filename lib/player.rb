654b29d45eb7306187ee6c9d3875db4f79eda561
Sylvain.D <sylvain.deligny@yahoo.fr>
2023-10-17 10:37:56 +0200

    gets dammages finished

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,16 @@
 class Player
   attr_accessor :name, :life_point
-  def initialize(name, life_point = 10)
+  def initialize(name, life_points = 10)
     @name = name
-    @life_point = life_point
+    @life_points = life_points
   end
-end
+  def show_state
+    puts "#{@name} a #{@life_points} points de vie"
+  end
+  def gets_damage(damage_took)
+    @life_points -= damage_took
+    @life_points <= 0 ? (puts "le joueur #{@name} a été Kill !") : show_state   # if @life_points <= 0
+  end                                                                           #  puts "le joueur #{@name} a été Kill !"
+end                                                                             #else
+                                                                                #  show_state
+                                                                                #end

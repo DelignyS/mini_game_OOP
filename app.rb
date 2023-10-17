9ec477315a625cae8a4fedf66c006339eafb8573
Sylvain.D <sylvain.deligny@yahoo.fr>
2023-10-17 14:32:49 +0200

    app_2 Initialized

--- app.rb
+++ app.rb
@@ -4,4 +4,24 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
+puts "À ma droite 'Josiane'"
+player1 = Player.new("Josiane")
+
+puts "À ma gauche 'José'"
+player2 = Player.new("José")
+
+puts "Voici l'état de nos joueurs :"
+player1.show_state
+player2.show_state
+
+while player1.life_points > 0 && player2.life_points > 0
+  puts "Passons à la phase d'attaque :"
+  player1.massive_attack(player2)
+  break if player2.life_points <= 0
+  player2.massive_attack(player1)
+  puts "Voici l'état de nos joueurs :"
+  player1.show_state
+  player2.show_state
+end
+
 binding.pry
--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,5 @@
 class Player
-  attr_accessor :name, :life_point
+  attr_accessor :name, :life_points
   def initialize(name, life_points = 10)
     @name = name
     @life_points = life_points
@@ -9,8 +9,17 @@ class Player
   end
   def gets_damage(damage_took)
     @life_points -= damage_took
-    @life_points <= 0 ? (puts "le joueur #{@name} a été Kill !") : show_state   # if @life_points <= 0
-  end                                                                           #  puts "le joueur #{@name} a été Kill !"
-end                                                                             #else
-                                                                                #  show_state
-                                                                                #end
+    @life_points <= 0 ? (puts "le joueur #{@name} a été Kill !") : show_state   # if @life_points <= 0   puts "le joueur #{@name} a été Kill !"  else show_state
+  end
+  def compute_damage
+    return rand(0..6)
+  end
+  def massive_attack(others)
+    puts "le joueur #{@name} attaque #{others.name}"
+    damages = compute_damage
+    others.gets_damage(damages)
+    puts "et il lui inflige #{damages} points de dommages"
+  end
+end                                                                             
+                                                                                
+                                                                      
